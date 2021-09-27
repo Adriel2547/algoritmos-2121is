fd04d7932d32c54dca273f593bbfa83d5ecc16c0
Adriel2547 <89948540+Adriel2547@users.noreply.github.com>
2021-09-27 12:29:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-var x = 50; //variable para comparar
-var y = 40;
-var s = 60;
- if ( x > y ){
-     console.log(x + " es mayor a " + y );
+// pasos para ir por tortillas
+var tortillas= "bicicleta";
+console.log("paso 1: agarrar la bolsa");
+console.log("paso 2: agarrar el dinero");
+if(tortillas = "bicicleta"){;
+console.log("paso3: agarramos la bicicleta");
+}else{;
+console.log("paso 3: vamos caminando");
+}
+console.log("paso 4: salir de la casa");
+console.log("paso 5: caminar o pedalear hasta la yortilleria");
+console.log("paso 6: formarse para comprar ");
+console.log("paso 7: comprar las tortillas");
+console,log("paso 8: pagar");
+console.log("paso 9: regresar a la casa");
+console.log("paso 10: entrar a la casa");
+console.log("paso 12: dejar las tortillas en la mesa")
